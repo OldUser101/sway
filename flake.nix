1c9efa5d2efe8201ffde9ebeb3d0f8f38ad40dac
Nathan Gill <nathan.j.gill@outlook.com>
2026-04-16 00:23:11 +0100

    dev: add nix flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            meson
+            ninja
+            pkg-config
+            wayland-scanner
+            scdoc
+
+            libGL
+            wayland
+            libxkbcommon
+            pcre2
+            json_c
+            libevdev
+            pango
+            cairo
+            libinput
+            gdk-pixbuf
+            librsvg
+            wayland-protocols
+            wlroots_0_19
+            libdrm
+            libxcb-wm
+          ];
+        };
+      });
+    };
+}
